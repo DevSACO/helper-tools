d5a8a16dfb20b8670573148b7f9ff818e2cf6256
Oscar C <44824137+devsaco-a@users.noreply.github.com>
2019-08-22 14:06:20 -0500

    Update remuxer.sh

--- remuxer.sh
+++ remuxer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$#" -ge "2" ]; then
  if [ "$1" == "-g" ]; then
-  if [ ! -x /usr/bin/gallery-dl -a ! -x /usr/bin/youtube-dl ]; then
+  if [ ! -x /usr/bin/gallery-dl -o ! -x /usr/bin/youtube-dl ]; then
    echo "Please install gallery-dl and youtube-dl first"; exit 0
   else
    if [ "$2" != "g" ]; then
-    if [ "$2" == "c" ]; then UDO="--sub-lang $5 --recode-video mkv --embed-subs"; fi
-    if [ "$3" == "a" ]; then PRO='-f bestaudio'; elif [ "$3" == "h" ]; then PRO='-f bestvideo+bestaudio'; fi; if [ "$4" != "" ]; then LAN="--playlist-start $4"; fi
+    if [ "$2" == "c" -a "$4" != "" ]; then UDO="--sub-lang $4 --recode-video mkv --embed-subs"; else echo "First enter language for embed"; fi
+    if [ "$3" == "a" ]; then PRO='-f bestaudio'; elif [ "$3" == "h" ]; then PRO='-f bestvideo+bestaudio'; fi; if [ $5 -ge 1 ]; then LAN="--playlist-start $5"; fi
     if [ "$3" == "r" ]; then
      echo "Getting VInfo"; youtube-dl -F -a lst-$2.txt >> .dlf 2>&1
      cat .dlf | grep -v 'hardsub' | grep 'x720' | sed 's|  [^\]*||' | sort | sed ':a;N;$!ba;s|\n|/|g' >> .formats; rm -R .dlf > /dev/null 2>&1
@@ -21,7 +21,6 @@ if [ "$#" -ge "2" ]; then
    head_args='-hide_banner -threads 0'
    video_scale_z='720'
    segment_seconds='60'
-   audio_args=' -c:a ac3 -b:a 320K -ar 48000 '
    audio_clean='silenceremove=0:0:0:-1:1:-50dB'
    audio_stereo="pan='stereo|FL<FL+0.5*FC+0.6*BL+0.6*SL|FR<FR+0.5*FC+0.6*BR+0.6*SR'"
    ### Start Audio container definitions
@@ -29,6 +28,9 @@ if [ "$#" -ge "2" ]; then
    ### Start Video container definitions
    if [ "$4" == "mp4" ]; then v_filter='mp4'; v_codec='libx264'; codec_options=' -b_strategy 1 -me_method hex -me_range 16 -partitions all -sc_threshold 40 '; p_format='yuvj444p'; v_bitrate='1M'; v_cfr='28'; e_out='mp4'; fi
    if [ "$4" == "mkv" ]; then v_filter='matroska'; v_codec='hevc'; p_format='yuv444p10le'; v_bitrate='1M'; v_cfr='20'; e_out='mkv'; fi
+   if [ "$4" == "mp3" ]; then audio_codec='-c:a mp3'; audio_ext='mp3'; fi
+   if [ "$4" == "m4a" ]; then audio_codec='-c:a ac3'; audio_ext='m4a'; fi
+   audio_args=" $audio_codec -b:a 320K -ar 48000 "
    video_args=" -f $v_filter -c:v $v_codec$codec_options -pix_fmt $p_format -g 250 -i_qfactor 0.71 -keyint_min 25 -subq 6 -preset veryslow -b:v $v_bitrate -crf $v_cfr -c:s copy"
    ### End Video container definitions
    ### Start Recusively list of all files in all subdirectories
@@ -49,12 +51,12 @@ if [ "$#" -ge "2" ]; then
     # Start volume normalization detection
     if [ "$5" == "n" ]; then
     audio_mvd="$(ffprobe -v 0 -show_entries stream=index,codec_type -of compact "$name_m" | grep -m 1 'audio' | sed 's|[^\]*index\=|-map 0:|;s|\|co[^\]*||')"
-    volume_dt="$(ffmpeg $head_args -i "$name_m" $audio_mvd -af volumedetect -f null -c:a ac3 -b:a 256k /dev/null 2>&1 | grep 'max_volume' | sed 's| ||g;s|\-||g;s|[^\]*:||')"; if [ ! -z "$volume_dt" ]; then cdl=','; modvol="volume=$volume_dt"; fi; audio_lang="$6"; subtitle_lang="$7"; else audio_lang="$5"; subtitle_lang="$6"; fi
+    volume_dt="$(ffmpeg $head_args -i "$name_m" $audio_mvd -af volumedetect -f null -c:a flac /dev/null 2>&1 | grep 'max_volume' | sed 's| ||g;s|\-||g;s|[^\]*:||')"; if [ ! -z "$volume_dt" ]; then dlms=','; modvol="volume=$volume_dt"; fi; audio_lang="$6"; subtitle_lang="$7"; else audio_lang="$5"; subtitle_lang="$6"; fi
     # End volume normalization detection
     # Start mode definitions
     if [ "$3" == "c" ]; then video_args=' -c:v copy'; audio_args=' -c:a copy '
-    elif [ "$3" == "p" ]; then audio_convert=" -af $audio_stereo$cdl$modvol"
-    elif [ "$3" == "s" ]; then audio_convert=" -af $audio_clean,$audio_stereo$cdl$modvol"
+    elif [ "$3" == "p" ]; then audio_convert=" -af $audio_stereo$dlms$modvol"
+    elif [ "$3" == "s" ]; then audio_convert=" -af $audio_clean,$audio_stereo$dlms$modvol"
     elif [ "$3" == "z" ]; then video_scale=" -vf scale=-1:$video_scale_z$(if [ "$5" == "n" ]; then printf " -af $modvol"; fi)"
     elif [ "$3" == "n" ]; then audio_convert=" -af $modvol" ; fi
     # End mode definitions
@@ -65,7 +67,7 @@ if [ "$#" -ge "2" ]; then
      for A in $(ffprobe -v 0 -show_entries stream=codec_type:stream_tags=language -of compact "$name_r" | grep 'audio' | sed "s|[^\]*tag:||;s|und|$audio_lang|"); do
       printf " -metadata:s:a:$((k++)) handler_name='Sound Media Handler' -metadata:s:a:$((l++)) encoder='Sound Media Encoder' -metadata:s:a:$((m++)) $A"
      done
-     printf "$split_args$audio_convert$audio_args:scq:Audio/$name_dir/$(if [ "$4" == "s" ]; then echo "$name_s"; fi)$splits$name_s.m4a:scq:"
+     printf "$split_args$audio_convert$audio_args:scq:Audio/$name_dir/$(if [ "$4" == "s" ]; then echo "$name_s"; fi)$splits$name_s.$audio_ext:scq:"
     elif [ "$2" == "v" ]; then
      if [ -e "$(echo ${name_r%.*}$subtitle_lang.ass)" ]; then printf " -i :scq:$(echo ${name_r%.*}$subtitle_lang.ass):scq:"; fi
      printf " -map_chapters -1 -map_metadata -1 $video_map $audio_map $subtt_map"
@@ -79,12 +81,12 @@ if [ "$#" -ge "2" ]; then
    ## End a/v media detection
    ### End Recusively list of all files in all subdirectories
    #### Start clear trash, sanitize and start remuxing
-   sed -i "s|\"|$(echo '\\''\"')|g;s|\!|$(echo '\"''\\''\!''\"')|g;s|:scq:|\"|g;s|:scs:| |g;s|:scp:|%|g;s|#/|#!/|;s|/\./|/|g;s|A_MTIME|/%05d - |;s|-metadata:s:a:[0-9] stream\|||g;s|-metadata:s:s:[0-9] stream\|||g;s|-metadata:s:v:[0-9] stream\|||g;s| codec_type=audio||g;s| codec_type=subtitle||g;s| codec_type=video||g;s|, | |;s| ,| |;s|\=.esLA|\=spa|g" .remuxer; printf "Remuxing\n"; . .remuxer; rm -R .remuxer
+   sed -i "s|\"|$(echo '\\''\"')|g;s|\!|$(echo '\"''\\''\!''\"')|g;s|:scq:|\"|g;s|:scs:| |g;s|:scp:|%|g;s|#/|#!/|;s|/\./|/|g;s|program\|||g;s|A_MTIME|/%05d - |;s|-metadata:s:a:[0-9] stream\|||g;s|-metadata:s:s:[0-9] stream\|||g;s|-metadata:s:v:[0-9] stream\|||g;s| codec_type=audio||g;s| codec_type=subtitle||g;s| codec_type=video||g;s|language= |language=eng |;s|\=.esLA|\=spa|g" .remuxer; printf "Remuxing\n"; . .remuxer; rm -R .remuxer
    #### End clear trash, sanitize and start remuxing script
-  fi
+  fi; printf "\n"
  fi
 else
- echo "Usage: ./remuxer.sh option type mode [extra options] [language]
+ echo "Usage: ./remuxer.sh option type mode media_type [extra options] [language]
   * Options
    -g			# Get media from filelist, use -g (name) mode [playlist num start]. file is [(name).ext].
    -c			# Convert media files.
@@ -93,15 +95,13 @@ else
    v			# Converts recusively massive video files in high deep subdir to matroska mkv, all streams are mapped in out file, except side data (in some cases).   
    + Mode
     c			# Copy codecs as original in out container
-    n			# If beta option \$5 = n
+    n			# Volume detects, and try normalize. (up/down in some cases work) 
     p			# Converts 7.1<= to stereo (Debug LFE)
     s			# Truncate silence and converts 7.1<= to stereo (Debug LFE). Changes dB in audio_clean option
     z			# Scale resolution. Changes into script in -vf scale option. Default 720p
-    + Extra options
+   + media type		# Specify mp3, m4a, mp4 or mkv
+   + Extra options
      s			# Split into (time)s multiple files. Changes into script in segment_seconds option. Default 60s. (Debug, only audio.)
-     media_type		# Specify mp4 or mkv
-     + Beta option
-      n			# Volume detects, and try normalize. (up/down in some cases work)
-      Language options
-       (language audio)	(language subtitle) # Only if streams have metadata language=und. (Debug)"
+   + Language options
+     (language audio) (language subtitle) # Only if streams have metadata language=und. (Debug)"
 fi
